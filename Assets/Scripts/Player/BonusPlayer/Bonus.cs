a76b43ec7d1d38b4999312fa4781886d10cac29c
Genicq Florian <floriangenicq@gmail.com>
2020-04-04 19:41:17 +0200

    Bonus Drop
    Logique de drop des bonus mise en place mais pas encore fonctionnel sur Unity

---
+++ Assets/Scripts/Player/BonusPlayer/Bonus.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bonus : MonoBehaviour
+{
+    protected int lifeTime;
+
+    public int LifeTime { get => lifeTime; set => lifeTime = value; }
+
+    protected void DropBonusMob(Enemy enemy,GameObject prefab)
+    {
+        Debug.Log(" PREFAB  " + prefab);
+        Instantiate(prefab, new Vector3(enemy.transform.position.x, enemy.transform.position.y, enemy.transform.position.z), Quaternion.identity);
+    }
+
+    /*protected void RandomDropBonus()
+    {
+        int chance = Random.Range(1, 100);
+
+        switch (chance)
+        {
+            case int n when (n <= 100):
+
+                DropBonusMob();
+                break;
+        }
+    } 
+
+    public void RandomBonus(Enemy ene)
+    {
+        enemy = ene;
+        int rdn = Random.Range(1, 2);
+
+        if(rdn == 1)
+        {
+            RandomDropBonus();
+        }
+    }*/
+}
---
+++ Assets/Scripts/Player/BonusPlayer/BonusHealth.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusHealth : Bonus
+{
+    public GameObject prefabHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LifeTime = 10;
+    }
+
+    public void DropBonus(Enemy enemy)
+    {
+        Debug.Log("PASSAGE HEALTH " + prefabHealth);
+        this.DropBonusMob(enemy,prefabHealth);
+    }
+
+}
